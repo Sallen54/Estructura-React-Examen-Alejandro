d618f3dcf0488d7f7ea2f80d52a23757bdd34ec4
Alejandro <alejandalmau@gmail.com>
2026-06-10 11:56:45 +0200

    Escomensant estuctura

--- Examen React/src/main.jsx
+++ Examen React/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {Router, createBrowserRouter} from "react-router-dom";
+import{RouterProvider} from 'react-router'
+import Router from './Router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router}/>
+
   </StrictMode>,
 )
